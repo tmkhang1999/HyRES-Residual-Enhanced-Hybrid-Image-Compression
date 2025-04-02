0586dbf738f93d5b249bbef603fda9e6ce38fc36
Khang Tran <74235084+tmkhang1999@users.noreply.github.com>
2025-04-02 03:39:37 +0200

    Add RateDistortionLoss and VGGLoss

---
+++ src/losses/rd_loss.py
@@ -0,0 +1,36 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class RateDistortionLoss(nn.Module):
+    """Custom rate distortion loss with a Lagrangian parameter."""
+
+    def __init__(self, lmbda=1e-2):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.lmbda = lmbda
+
+    def forward(self, output, target):
+        N, _, H, W = target.size()
+        out = {}
+        num_pixels = N * H * W
+
+        out["bpp_loss"] = sum(
+            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
+            for likelihoods in output["likelihoods"].values()
+        )
+        out["y_bpp_loss"] = sum(
+            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
+            for likelihoods in output["likelihoods"]["y"]
+        )
+        out["z_bpp_loss"] = sum(
+            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
+            for likelihoods in output["likelihoods"]["z"]
+        )
+        out["mse_loss"] = self.mse(output["x_hat"], target) * 255 ** 2
+        out["loss"] = self.lmbda * out["mse_loss"] + out["bpp_loss"]
+
+        return out
+
