31b8b420377460eb1244a5ba0de17da184f74c80
Khang Tran <74235084+tmkhang1999@users.noreply.github.com>
2025-04-20 22:23:39 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,19 +2,32 @@ python -m src.training \
   -d ./data \
   --N 128 \
   --M 192 \
-  --jpeg-quality 10 \
+  --jpeg-quality 1 \
   --patch-size 256 256 \
   --batch-size 16 \
   --test-batch-size 16 \
   -e 1000 \
-  -lr 2e-4 \
+  -lr 1e-3 \
   --aux-learning-rate 1e-3 \
   -n 4 \
-  --lambda 13e-3 \
+  --lambda 0.032 \
   --cuda True\
   --save \
   --seed 1926 \
-  --clip_max_norm 1.0
+  --clip_max_norm 1.0 \
+  --mixed-precision \
+  --gradient-accumulation-steps 2 \
+  --savepath ./checkpoint/phase1
 
 #  --pretrained \
 #  --checkpoint Pretrained4000epoch_checkpoint.pth.tar
+
+## Phase 1: mae_loss
+# 3e-4
+# 0.032
+
+## Phase 2: bpp_loss (reduce 0.19 + 0.2 -> 0.4)
+# 1e-3
+# 0.016
+
+
