b0cffe38895a33f48ec40783e84dacd5fad6f29f
Khang Tran <74235084+tmkhang1999@users.noreply.github.com>
2025-05-16 00:42:59 +0200

    Create updata.sh

---
+++ updata.sh
@@ -0,0 +1,7 @@
+python -m src.updata \
+  --filepath ./checkpoint/phase1/checkpoint_best_loss_98.pth.tar \
+  --name phase1_0.032 \
+  --dir ./checkpoint/inference \
+  --N 128 \
+  --M 192 \
+  --jpeg-quality 1
